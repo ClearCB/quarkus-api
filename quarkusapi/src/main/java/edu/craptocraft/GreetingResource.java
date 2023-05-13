957ab096a29bd243e4fcba458eeaf6e739f1202e
Abel Casas <abelcasasccb@gmail.com>
2023-05-13 16:55:33 +0200

    build(META-INF/services): add new configuration base

--- quarkusapi/src/main/java/edu/craptocraft/GreetingResource.java
+++ quarkusapi/src/main/java/edu/craptocraft/GreetingResource.java
@@ -1,5 +1,7 @@
 package edu.craptocraft;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,12 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greetings.message")
+    String msg;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return msg;
     }
 }
---
+++ quarkusapi/src/main/java/edu/craptocraft/InMemoryConfigSource.java
@@ -0,0 +1,42 @@
+package edu.craptocraft;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+public class InMemoryConfigSource implements ConfigSource {
+    private Map<String, String> prop = new HashMap<>();
+
+    public InMemoryConfigSource() {
+
+        this.prop.put("greetings.message", "Memoty Hello");
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return prop;
+    }
+
+    @Override
+    public int getOrdinal() {
+        return 900;
+    }
+
+    @Override
+    public String getName() {
+        return "InMemoryConfigSource";
+    }
+
+    @Override
+    public String getValue(String propertyName) {
+        return prop.get(propertyName);
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return prop.keySet();
+    }
+
+}
