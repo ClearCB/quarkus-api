201e79e156c34817d592f05ea81ba10b0106085b
ClearCB <abelcasasccb@gmail.com>
2023-05-15 16:00:32 +0200

    casas

--- quarkusapi/src/main/java/edu/craptocraft/resources/GreetingResourceNow.java
+++ quarkusapi/src/main/java/edu/craptocraft/resources/GreetingResourceNow.java
@@ -1,11 +1,9 @@
 package edu.craptocraft.resources;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.logging.Logger;
 
-import edu.craptocraft.models.Beer;
 import edu.craptocraft.models.WorldClock;
-import edu.craptocraft.services.GreetingServices;
+import edu.craptocraft.models.WorldClockHeaders;
 import edu.craptocraft.services.WorldClockService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -31,6 +29,11 @@ public class GreetingResourceNow {
          * And pass to the method this object:
          * return worldClockService.getNow(worldClockHeaders);
          */
+
+        WorldClockHeaders worldClockHeaders = new WorldClockHeaders();
+        worldClockHeaders.logger = "DEBUG";
+        return worldClockService.getNow(worldClockHeaders);
+
         return worldClockService.getNow();
     }
 
