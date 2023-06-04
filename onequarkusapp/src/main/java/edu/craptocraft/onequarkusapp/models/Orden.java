27c0d8425e190d51cb9cc63b1876b1621e41b2a6
Abel Casas <abelcasasccb@gmail.com>
2023-06-05 01:02:33 +0200

    ag

--- onequarkusapp/src/main/java/edu/craptocraft/onequarkusapp/models/Orden.java
+++ onequarkusapp/src/main/java/edu/craptocraft/onequarkusapp/models/Orden.java
@@ -3,12 +3,10 @@ package edu.craptocraft.onequarkusapp.models;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -25,7 +23,7 @@ public class Orden extends PanacheEntityBase {
     @JoinColumn(name = "ord_user")
     private Usuaria usuaria;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "ord_item")
     private Item item;
 
